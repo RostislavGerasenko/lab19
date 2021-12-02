68ee7005c47d202f90b0f7fc120b09fc029e665f
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-02 11:34:08 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 n = int(input("Size of the array: "))
-arr = [int(input(f"Element number {i+1} of the first array: ")) for i in range(n)]
+arr = [int(input(f"Element number {i+1} of the array: ")) for i in range(n)]
 minimum = min(arr)
 maximum = max(arr)
 arr.insert(arr.index(minimum), 0)
