d45954b09fcf37d53ad43c1c913d9c174e771fe6
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-02 11:35:33 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+n = int(input("Size of the array: "))
+arr = [int(input(f"Element number {i+1} of the array: ")) for i in range(n)]
+for index, element in enumerate(arr):
+    if element < 0:
+        arr.insert(index + 1, 0)
+print(f"Array: {arr}")
