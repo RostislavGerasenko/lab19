db0060ee2e797cce960c266d31ce57202b12681d
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-02 11:33:52 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 n = int(input("Size of the array: "))
-arr = [int(input(f"Element number {i+1} of the first array: ")) for i in range(n)]
+arr = [int(input(f"Element number {i+1} of the array: ")) for i in range(n)]
 for i in range(len(arr)-1, 0, -1):
     if arr[i] == arr[i-1]:
         del arr[i]
