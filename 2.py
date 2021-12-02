552d6a056b1a66da4d81d1acf4e67a8b65263a78
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-02 11:33:59 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 n = int(input("Size of the array: "))
-arr = [int(input(f"Element number {i+1} of the first array: ")) for i in range(n)]
+arr = [int(input(f"Element number {i+1} of the array: ")) for i in range(n)]
 count = Counter(arr)
 for item, amount in count.items():
     if amount == 2:
