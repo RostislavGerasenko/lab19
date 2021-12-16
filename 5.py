e15b465338765599bce5c8fd820b221bff1e1b9f
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-16 11:16:31 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,6 +1,12 @@
 n = int(input("Size of the array: "))
 arr = [int(input(f"Element number {i+1} of the array: ")) for i in range(n)]
+flag = False
 for index, element in enumerate(arr):
+    if flag:
+        flag = False
+        continue
     if element > 0:
+        flag = True
         arr.insert(index, 0)
+        index += 1
 print(f"Array: {arr}")
